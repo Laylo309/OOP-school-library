6c3505e1b3e8f7913ca1acfc1adc40dc6fb42d30
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-11-16 05:12:29 +0500

    Add classes

---
+++ person.rb
@@ -0,0 +1,20 @@
+class Person
+  def initialize(age:, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def can_use_services?
+    of_age? || true
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
---
+++ teacher.rb
@@ -0,0 +1,16 @@
+require_relative './person'
+
+class Teacher < Person
+  def initialize(age:, name: 'Unknown', parent_permission: true)
+    super(age: age, name: name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
+teacher = Teacher.new(name: 'Myname', age: 15, parent_permission: false, specialization: 'Doctor')
+
+puts teacher.can_use_services?
