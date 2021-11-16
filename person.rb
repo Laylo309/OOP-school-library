5adc569f49727872c4e9c0a56921462e6c6e6348
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-11-16 06:40:59 +0500

    Follow Ruby Best practiceses

--- person.rb
+++ person.rb
@@ -1,12 +1,13 @@
 class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
   def initialize(age:, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-  attr_reader :id
-  attr_accessor :name, :age
 
   def can_use_services?
     of_age? || true
