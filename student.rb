60a2ef2ccacaf1e3e4f0f82c1b1e07904e1c771b
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-11-19 00:54:19 +0500

    Fix  linter errors

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name = 'Unknown', parent_permission = true, classroom)
+  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 
 class Teacher < Person
-  def initialize(age, name = 'Unknown', parent_permission = true, specialization)
+  def initialize(age, specialization, name: 'Unknown', parent_permission: true)
     super(age: age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
@@ -9,5 +9,4 @@ class Teacher < Person
   def can_use_services?
     true
   end
-
 end
