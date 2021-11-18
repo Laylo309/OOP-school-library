0b2aff2328939c9f4fc41616343e225380c7bb16
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-11-19 00:46:49 +0500

    Fix linters

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age:, name = 'Unknown', parent_permission = true, classroom:)
+  def initialize(age, name = 'Unknown', parent_permission = true, classroom)
     super(age, name, parent_permission)
     @classroom = classroom
   end
--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 
 class Teacher < Person
-  def initialize(age:, name = 'Unknown', parent_permission = true, specialization:)
+  def initialize(age, name = 'Unknown', parent_permission = true, specialization)
     super(age: age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
@@ -9,8 +9,5 @@ class Teacher < Person
   def can_use_services?
     true
   end
+
 end
-
-teacher = Teacher.new(name: 'Myname', age: 15, parent_permission: false, specialization: 'Doctor')
-
-puts teacher.can_use_services?
